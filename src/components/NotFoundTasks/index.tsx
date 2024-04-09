c19d59d8adef200c90f782cba40c0c5a75aaedc0
Mateus Duarte <mateus_duarte19@outlook.com>
2024-04-09 04:27:52 -0300

    feat: Add not found tasks component and clipboard image

---
+++ src/components/NotFoundTasks/index.tsx
@@ -0,0 +1,20 @@
+import { Image, Text, View } from "react-native";
+import { styles } from "./styles";
+
+export const NotFoundTasks = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../../assets/clipboard.png")}
+        width={56}
+        height={56}
+      />
+      <Text style={styles.descriptionOne}>
+        Você ainda não tem tarefas cadastradas
+      </Text>
+      <Text style={styles.descriptionTwo}>
+        Crie tarefas e organize seus itens a fazer
+      </Text>
+    </View>
+  );
+};
---
+++ src/components/NotFoundTasks/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 48,
+  },
+  descriptionOne: {
+    marginTop: 16,
+    fontFamily: "Inter-Bold",
+    fontSize: 14,
+    color: "#808080",
+    textAlign: "center",
+  },
+  descriptionTwo: {
+    fontFamily: "Inter-Regular",
+    fontSize: 14,
+    color: "#808080",
+    textAlign: "center",
+  },
+});
