29ca38a4bd3f17c462c81fe044a13f3a0080c48a
Mateus Duarte <mateus_duarte19@outlook.com>
2024-04-09 00:42:03 -0300

    [build|feat]: Add expo fonts and configs -- Add expo-fonts and expo-splash-screen libraries -- Add Inter regular and bold Fonts -- Add useFonts and autohideasync in home page to make the fonts work

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,9 +3,31 @@ import { styles } from './styles'
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
+
+SplashScreen.preventAutoHideAsync();
+
 export const Home = () => {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Regular': require('../../../assets/fonts/inter-regular.otf'),
+    'Inter-Bold': require('../../../assets/fonts/inter-bold.otf')
+  });
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Header />
       <View style={styles.content}>
        <Input />
